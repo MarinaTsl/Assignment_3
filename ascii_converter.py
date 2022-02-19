b45e78b5e02cbc5f2c4d7ffa46a8a12c9bb29c3b
MarinaTsl <marina.tsalavra@gmail.com>
2022-02-19 20:37:17 +0200

    Added a missing (

--- ascii_converter.py
+++ ascii_converter.py
@@ -12,11 +12,11 @@ for i in range(len(words)):
         words.pop(words[i])       
 
     #Else, if it is a word shorter than 20 characters: 
-    elif (i>0 and len(words[i])<20]):
+    elif (i>0 and len(words[i])<20):
         #Search the already checked part of the list 
         for k in range(i):
            #for a word whose length plus the current word's length is 20
-            if(len(words[i]+len(words[k])==20):
+            if(len(words[i])+len(words[k])==20):
                 #If found, pop the word
                 words.pop(words[i]); words.pop(words[k])
                 break
